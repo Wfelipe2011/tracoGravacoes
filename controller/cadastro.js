ba2b0caa38f65bacf38fe14618d3af67947f0aca
Wilson Felipe <wfelipe2011@gmail.com>
2021-06-30 12:34:09 -0300

    estruta css e estilos cadastro e checkin

---
+++ controller/cadastro.js
@@ -0,0 +1,34 @@
+import { service } from "../service/index.js"
+import { view } from "../view/index.js"
+
+export const CadastroComponent = () => {
+    const label = []
+    service.getVeiculo().then((dados)=> {
+        dados.forEach(element => {
+            if(element.label != null){
+                label.push(element.label)
+            }
+        });
+    })
+    
+    view.getCadastroHtml();
+
+    const formulario = document.getElementById("formulario")
+    formulario.addEventListener('submit', function (event) {
+        event.preventDefault()
+
+        const cadastroCliente = {
+            owner: document.getElementById('name').value,
+            model: document.getElementById('modelo').value,
+            type: document.getElementById('tipo').value,
+            label: document.getElementById('placa').value,
+            observation: document.getElementById('observacoes').value
+        }
+
+        if(label.includes(cadastroCliente.label)){
+            return alert(`Essa placa: ${cadastroCliente.label} já existe no banco.`)
+        }else {
+            service.postVeiculo(cadastroCliente)
+        }
+    })
+}
